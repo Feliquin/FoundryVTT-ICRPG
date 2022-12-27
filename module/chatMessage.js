15261d4bbe151b04d8ce81619e2ecacbca3294eb
Feliquin <tommy@feliquin.de>
2022-12-27 23:52:38 +0100

    bugfixing after update 5

--- module/chatMessage.js
+++ module/chatMessage.js
@@ -21,32 +21,5 @@ export class IcrpgChatMessage extends ChatMessage {
         await super._preCreate(data, options, user)
     }
 
-    async getHTML() {
-        console.log('============> TEST');
-        const html = await super.getHTML();
-        console.log('HTML', html);
-        
-        const icrpgFlags = this.flags.icrpg || {};
-        const passInFlags = "pass" in icrpgFlags;
-        const failInFlag = "fail" in icrpgFlags;
-        const criticalInFlag = "critical" in icrpgFlags;
-
-        if (!passInFlags) return html;
-
-        if (this.flags.icrpg.fail) {
-            html.find(".flavor-text").addClass("icrpg-totalfail-text");
-            html.find("h4.dice-total").addClass("icrpg-totalfail");
-        } else if (this.flags.icrpg.critical) {
-            html.find(".flavor-text").addClass("icrpg-critical-text");
-            html.find("h4.dice-total").addClass("icrpg-critical");
-        } else if (this.flags.icrpg.pass) {
-            html.find(".flavor-text").addClass("icrpg-pass-text");
-            html.find("h4.dice-total").addClass("icrpg-pass");
-        } else {
-            html.find(".flavor-text").addClass("icrpg-fail-text");
-            html.find("h4.dice-total").addClass("icrpg-fail");
-        }
-
-        return html;
-    }
+    
 }
